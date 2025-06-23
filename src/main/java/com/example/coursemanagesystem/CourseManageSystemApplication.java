7cd2802f8146084b23f39f9c80cd2ef580cdf749
SmallCarrotQWQ <395680955@qq.com>
2025-06-23 15:02:23 +0800

    首次提交

---
+++ src/main/java/com/example/coursemanagesystem/CourseManageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursemanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursemanagesystem/CourseManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursemanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
